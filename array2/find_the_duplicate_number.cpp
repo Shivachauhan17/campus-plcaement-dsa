529629df7bb93fa32570ce9f5ac5cec8f1411a12
Shivachauhan17 <shivanbd2019@gmail.com>
2023-08-20 06:33:07 +0530

    fin_the_duplicate_number

---
+++ array2/find_the_duplicate_number.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// 1.) Hashing :- t.c.->O(n), s.c. -> O(n)
+int duplicate(vector<int> arr){
+    if(arr.size()==0) return -1;
+    int n=arr.size();
+    vector<int> ans(n);
+    for(int i=0;i<n;i++){
+        ans[arr[i]]+=1;
+    }
+    for(int i=0;i<n;i++){
+        if(ans[i]>1) return i;
+    }
+    return -1;
+}
+
+
+// Linked List :- t.c.->O(n), s.c.->O(1) 
+//pending
+
+int main(){
+    vector<int> arr={2,5,9,6,9,3,8,9,7,1};
+    cout<<duplicate(arr)<<endl;
+}
