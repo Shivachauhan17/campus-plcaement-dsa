543d8d9918887d470a6e3cc7908c9872c1f2239a
Shivachauhan17 <shivanbd2019@gmail.com>
2023-08-27 18:18:14 +0530

    ->

---
+++ linked-list/merge_two_sorted_linked_list.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+
+class Node {
+public:
+    int data;
+    Node* next;
+};
+
+Node* merge(Node* a,Node* b){
+    Node dummy;
+    Node* tail=&dummy;
+    if(a==NULL){
+        tail->next=b;
+        return dummy.next;
+    }
+    if(b==NULL){
+        tail->next=a;
+        return dummy.next;
+    }
+
+    while(1){
+        if(a->data<b->data){
+            
+        }
+    }
+    
+}
+
+ int main(){
+   ListNode* ListNode(1)
+ }
+ 
