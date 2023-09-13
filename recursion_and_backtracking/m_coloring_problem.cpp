b7fab166ab0c514f379c3a749c0e42feebc44a16
Shivachauhan17 <shivanbd2019@gmail.com>
2023-09-13 12:46:14 +0530

    n queens and sudoku

---
+++ recursion_and_backtracking/m_coloring_problem.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//same like n queen check colors for every node and backtrack
+
+int main(){
+    int n=4;
+    int m=3;
+
+    bool graph[101][101];
+    memset(graph, false, sizeof graph);
+
+  // Edges are (0, 1), (1, 2), (2, 3), (3, 0), (0, 2)
+  graph[0][1] = 1; graph[1][0] = 1;
+  graph[1][2] = 1; graph[2][1] = 1;
+  graph[2][3] = 1; graph[3][2] = 1;
+  graph[3][0] = 1; graph[0][3] = 1;
+  graph[0][2] = 1; graph[2][0] = 1;
+  
+  cout << graphColoring(graph, m, N);
+}
--- recursion_and_backtracking/n_queens.cpp
+++ recursion_and_backtracking/n_queens.cpp
@@ -1,59 +1,77 @@
-#include<iostream>
-#include<vector>
+#include <bits/stdc++.h>
+
 using namespace std;
+class Solution {
+  public:
+    bool isSafe1(int row, int col, vector < string > board, int n) {
+      // check upper element
+      int duprow = row;
+      int dupcol = col;
 
+      while (row >= 0 && col >= 0) {
+        if (board[row][col] == 'Q')
+          return false;
+        row--;
+        col--;
+      }
 
-bool issafe(vector<vector<int>> board,int row ,int col){
-    for(int i=0;i<col;i++){
-        if(board[i][col]){
-            return false;
+      col = dupcol;
+      row = duprow;
+      while (col >= 0) {
+        if (board[row][col] == 'Q')
+          return false;
+        col--;
+      }
+
+      row = duprow;
+      col = dupcol;
+      while (row < n && col >= 0) {
+        if (board[row][col] == 'Q')
+          return false;
+        row++;
+        col--;
+      }
+      return true;
+    }
+
+  public:
+    void solve(int col, vector < string > & board, vector < vector < string >> & ans, int n) {
+      if (col == n) {
+        ans.push_back(board);
+        return;
+      }
+      for (int row = 0; row < n; row++) {
+        if (isSafe1(row, col, board, n)) {
+          board[row][col] = 'Q';
+          solve(col + 1, board, ans, n);
+          board[row][col] = '.';
         }
+      }
     }
 
-    int maxleft=min(row,col);
-    for(int i=1;i<=maxleft;i++){
-        if(board[row-i][col-i]){
-            return false;
-        }
+  public:
+    vector < vector < string >> solveNQueens(int n) {
+      vector < vector < string >> ans;
+      vector < string > board(n);
+      string s(n, '.');
+      for (int i = 0; i < n; i++) {
+        board[i] = s;
+      }
+      solve(0, board, ans, n);
+      return ans;
     }
-
-    int newcol=board.size()-col-1;
-    int maxright=min(row,newcol);
-
-    for(int i=1;i<=maxright;i++){
-        if(board[row-i][col+i]){
-            return false;
-        }
-    } 
-
-    return true;
-}
-
-int queen(vector<vector<int>> board,int row){
-    if(row==board.size()){
-        return 1;
+};
+int main() {
+  int n = 4; // we are taking 4*4 grid and 4 queens
+  Solution obj;
+  vector < vector < string >> ans = obj.solveNQueens(n);
+  for (int i = 0; i < ans.size(); i++) {
+    cout << "Arrangement " << i + 1 << "\n";
+    for (int j = 0; j < ans[0].size(); j++) {
+      cout << ans[i][j];
+      cout << endl;
     }
-
-    int count=0;
-    for(int col=0;col<board.size();col++){
-        if(issafe(board,row,col))
-            board[row][col]=true;
-        count+=queen(board,row+1);
-        board[row][col]=false;
-    }
-    return count;   
-}
-
-
-
-
-
-int main(){
-    vector<vector<int>> board={ { 0, 0, 0, 0 },
-                        { 0, 0, 0, 0 },
-                        { 0, 0, 0, 0 },
-                        { 0, 0, 0, 0 } };
-
-    int count=queen(board,0);
-    cout<<count;
+    cout << endl;
+  }
+  return 0;
 }
